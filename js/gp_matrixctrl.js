98cd362adbe3567cd45884fc308c3628e7c7588d
Gregg Oliva <gloliva@proton.me>
2023-10-03 14:43:30 -0700

    Move js and json files to their own directories

---
+++ js/gp_matrixctrl.js
@@ -0,0 +1,8 @@
+/*
+    Handles formatting messages to send to `thispatcher` object for
+    dynamically creating groove~ objects and connecting them to a `route` object.
+*/
+
+inlets = 2;
+outlets = 1;
+autowatch = 1;
